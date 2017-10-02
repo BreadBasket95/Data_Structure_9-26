a89382040135bf027841c4db5b4f804a30ae63a6
Marc Moore <mnm419@msstate.edu>
2017-10-01 22:43:40 -0500

    Started the Convert function Currently not functional.

--- driver_TestConvFns.cpp
+++ driver_TestConvFns.cpp
@@ -13,7 +13,9 @@
 
 #include "Node.h"
 #include "Stack.h"
+#include "Stack.cpp"
 #include "Queue.h"
+#include "Queue.cpp"
 
 using namespace std;
 
@@ -43,4 +45,52 @@ void test() {
   cout << convert_to_pf(")2*(2-7)/(4") << endl;
   cout << "******************************************\n\n";
 
+}
+
+string convert_to_pf(string eq)
+{
+  Stack stackone; // The stack object that will help sort the string tokens
+  Queue queueone; // The queue object that will store the final expression
+
+  for(int count = 0; count < eq.length(); count++){
+
+    if(isdigit(eq[count])){
+      queueone.enqueue(eq[count], 3 );
+  }
+    else if(eq[count]= "(" {
+      stackone.push(eq[count])
+    }
+    else if()
+
+/*  create an empty stack
+  create an empty queue to represent the postfix expression
+  for each token in the expression:
+  if token is a number:
+  append it to the postfix expression
+  elif token is a left parenthesis:
+  push it to the stack
+
+  elif token is an operator:
+  while (stack is not empty and the top stack
+  item is an operator
+  with precedence greater than or equal to token):
+  pop and append the operator
+  to the postfix expression
+  push the token onto the stack
+
+  else token must be a right parenthesis
+  while(the top item on the stack is not a left parenthesis)
+  :
+  pop item
+  from the stack and append it
+  to the postfix expression
+  pop the left parenthesis
+  while the stack is not empty
+  pop an item
+  from the stack and append it
+  to the postfix expression */
+  //string eq
+  //Queue queue_one;
+  //Stack stack_one;
+return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include "Node.h"
 #include "Queue.h"
+#include "Queue.cpp"
 #include "Stack.h"
+#include "Stack.cpp"
 
+string convert_to_pf(string eq);
 
 int main()
 {
